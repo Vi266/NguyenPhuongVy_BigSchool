bb59029c52d976c6ecd5ce79c12248c57bb57702
PHUONGVY <PHUONGVY@VII>
2022-03-22 15:58:17 +0700

    Add project files.

---
+++ 1911065716_NguyenPhuongVy_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1911065716_NguyenPhuongVy_BigSchool.Startup))]
+namespace _1911065716_NguyenPhuongVy_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
